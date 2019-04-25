51ff691891e7ca69593a15115d1b2025f7a0211a
Carlos Saito <exacs@users.noreply.github.com>
2019-04-25 14:01:29 +0200

    Implement AugmentedIterator.take

--- test/lib.js
+++ test/lib.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const augmentGenerator = require('../lib/augmentGenerator')
 
 test('augmentGenerator does not mutate the original generator', t => {
-  function * gen () {
+  async function * gen () {
     yield 1
   }
 
@@ -11,14 +11,24 @@ test('augmentGenerator does not mutate the original generator', t => {
   t.falsy(gen().take)
 })
 
-test('augmentGenerator returns a valid generator', t => {
-  function * gen () {
+test('augmentGenerator returns a valid generator', async t => {
+  async function * gen () {
     yield 1
   }
 
   const g2 = augmentGenerator(gen)
 
-  for (const v of g2()) {
+  for await (const v of g2()) {
     t.is(v, 1)
   }
+})
+
+test('AugmentedIterator.take works without arguments', async t => {
+  const gen = augmentGenerator(async function * () {
+    yield 1
+    yield 2
+    yield 3
+  })
+
+  t.deepEqual(await gen().take(), [1, 2, 3])
 })
