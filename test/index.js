a3f690b9d5aadb82fe7e1849591e34fcb917235f
Carlos Saito <exacs@users.noreply.github.com>
2019-05-02 13:05:42 +0200

    Replace mocking with create-test-server

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 const proxyquire = require('proxyquire').noCallThru()
-
 const test = require('ava')
+const createTestServer = require('create-test-server')
+
 const Canvas = require('../index')
 
 test('Token is correctly stripped', async t => {
@@ -16,31 +17,25 @@ test('Token is correctly stripped', async t => {
 })
 
 test('URLs are correctly "resolved"', async t => {
-  const urls = [
-    { base: 'http://example.com', end: '/index', expected: 'http://example.com/index' },
-    { base: 'http://example.com', end: 'index', expected: 'http://example.com/index' },
-    { base: 'http://example.com/', end: '/index', expected: 'http://example.com/index' },
-    { base: 'http://example.com/', end: 'index', expected: 'http://example.com/index' },
+  const server = await createTestServer()
+  server.get('/index', 'foo')
+  server.get('/api/v1/courses/1', 'foo')
 
-    { base: 'http://example.com/api/v1', end: '/courses/1', expected: 'http://example.com/api/v1/courses/1' },
-    { base: 'http://example.com/api/v1', end: 'courses/1', expected: 'http://example.com/api/v1/courses/1' },
-    { base: 'http://example.com/api/v1/', end: '/courses/1', expected: 'http://example.com/api/v1/courses/1' },
-    { base: 'http://example.com/api/v1/', end: 'courses/1', expected: 'http://example.com/api/v1/courses/1' }
+  const urls = [
+    { base: server.url, end: '/index' },
+    { base: server.url, end: 'index' },
+    { base: server.url + '/', end: '/index' },
+    { base: server.url + '/', end: 'index' },
+    { base: `${server.url}/api/v1`, end: '/courses/1' },
+    { base: `${server.url}/api/v1`, end: 'courses/1' },
+    { base: `${server.url}/api/v1/`, end: '/courses/1' },
+    { base: `${server.url}/api/v1/`, end: 'courses/1' }
   ]
 
-  for (const { base, end, expected } of urls) {
-    let spy
-    const SpecialCanvas = proxyquire('../index', {
-      'request-promise': function (obj) {
-        spy = obj.url
-        return []
-      }
-    })
-
-    const canvas = SpecialCanvas(base)
-    canvas.get(end)
-
-    t.is(spy, expected)
+  for (const { base, end } of urls) {
+    const canvas = Canvas(base, '')
+    const result = await canvas.get(end)
+    t.is(result.body, 'foo')
   }
 })
 
