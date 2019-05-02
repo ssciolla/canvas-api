d8267be69786f05ee384b601dd78081e45499649
Carlos Saito <exacs@users.noreply.github.com>
2019-05-02 13:39:46 +0200

    Replace proxyquire with create-test-server

--- test/index.js
+++ test/index.js
@@ -1,4 +1,3 @@
-const proxyquire = require('proxyquire').noCallThru()
 const test = require('ava')
 const createTestServer = require('create-test-server')
 
@@ -59,46 +58,29 @@ test('List returns a correct iterable', async t => {
 })
 
 test('List returns an Augmented iterable', async t => {
-  const SpecialCanvas = proxyquire('../index', {
-    'request-promise': function ({ url }) {
-      if (url === 'http://example.com/something') {
-        return {
-          body: [1, 2, 3],
-          headers: {
-            link: '<http://example.com/something_else>; rel="next", <irrelevant>; rel="first"'
-          }
-        }
-      } else if (url === 'http://example.com/something_else') {
-        return {
-          body: [4, 5]
-        }
-      }
-    }
-  })
+  const server = await createTestServer()
 
-  const canvas = SpecialCanvas('http://example.com')
+  server.get('/something', (req, res) => {
+    res.set('Link', `<${server.url}/something_else>; rel="next", <irrelevant>; rel="first"`)
+    res.send([1, 2, 3])
+  })
+  server.get('/something_else', [4, 5])
+
+  const canvas = Canvas(server.url, '')
   const result = await canvas.list('/something').toArray()
 
   t.deepEqual(result, [1, 2, 3, 4, 5])
 })
 
 test('List ignores non-"rel=next" link headers', async t => {
-  const SpecialCanvas = proxyquire('../index', {
-    'request-promise': function ({ url }) {
-      if (url === 'http://example.com/something') {
-        return {
-          body: [1],
-          headers: {
-            link: '<http://dont-call.com>; rel="last", <http://ignore-this.se>; rel="prev", <http://nope.com>; rel="first"'
-          }
-        }
-      } else {
-        t.fail(`The url: "${url}" was requested and should not be!`)
-      }
-    }
+  const server = await createTestServer()
+
+  server.get('/something', (req, res) => {
+    res.set('Link', '<http://dont-call.com>; rel="last", <http://ignore-this.se>; rel="prev", <http://nope.com>; rel="first"')
+    res.send([1])
   })
 
-  const canvas = SpecialCanvas('http://example.com')
+  const canvas = Canvas(server.url, '')
   const result = []
 
   for await (const e of canvas.list('/something')) {
@@ -108,29 +90,25 @@ test('List ignores non-"rel=next" link headers', async t => {
 })
 
 test('List can handle pagination urls with query strings', async t => {
-  const SpecialCanvas = proxyquire('../index', {
-    'request-promise': function ({ url, qs }) {
-      if (url === 'http://example.com/something') {
-        return {
-          body: [1, 2, 3],
-          headers: {
-            link: '<http://example.com/something_else?query=string>; rel="next", <irrelevant>; rel="first"'
-          }
-        }
-      } else if (url === 'http://example.com/something_else?query=string' && !qs) {
-        return {
-          body: [4, 5]
-        }
-      }
+  const server = await createTestServer()
+
+  server.get('/something', (req, res) => {
+    res.set('Link', `<${server.url}/something_else?query=string>; rel="next"`)
+    res.send([1])
+  })
+  server.get('/something_else', (req, res) => {
+    if (req.originalUrl === `/something_else?query=string`) {
+      res.send(['correct'])
+    } else {
+      res.send(['nope'])
     }
   })
 
-  const canvas = SpecialCanvas('http://example.com')
-  const result = []
+  const canvas = Canvas(server.url, '')
 
-  for await (const e of canvas.list('/something')) {
-    result.push(e)
-  }
+  const it = canvas.list('/something?with=query_string')
+  await it.next()
+  const result = await it.next()
 
-  t.deepEqual(result, [1, 2, 3, 4, 5])
+  t.is(result.value, 'correct')
 })
