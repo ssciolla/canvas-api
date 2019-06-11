a0b5b260254ef919ddb8e3fd499480921830f0c9
Carlos Saito <carlos@saito.style>
2019-06-11 17:08:03 +0200

    Replace manual URL resolution with Got internal

--- index.js
+++ index.js
@@ -18,13 +18,6 @@ function getNextUrl (linkHeader) {
 module.exports = (apiUrl, apiKey, options = {}) => {
   const log = options.log || (() => {})
 
-  const resolve = endpoint => {
-    const base2 = apiUrl.slice(-1) === '/' ? apiUrl.slice(0, -1) : apiUrl
-    const endpoint2 = endpoint.charAt(0) === '/' ? endpoint.slice(1) : endpoint
-
-    return base2 + '/' + endpoint2
-  }
-
   const canvasGot = got.extend({
     headers: {
       'Authorization': `Bearer ${apiKey}`
@@ -33,11 +26,11 @@ module.exports = (apiUrl, apiKey, options = {}) => {
   })
 
   async function requestUrl (endpoint, method = 'GET', body = {}, options = {}) {
-    const url = resolve(endpoint)
+    log(`Request ${method} ${endpoint}`)
 
-    log(`Request ${method} ${url}`)
     try {
       const result = await canvasGot({
+        baseUrl: apiUrl,
         body: body,
         url,
         method,
@@ -52,10 +45,12 @@ module.exports = (apiUrl, apiKey, options = {}) => {
   }
 
   async function get (endpoint, queryParams = {}) {
-    const url = resolve(endpoint)
-
-    const query = queryString.stringify(queryParams, {arrayFormat: 'bracket'})
-    return canvasGot({url, method: 'GET', query})
+    return canvasGot({
+      url: endpoint,
+      baseUrl: apiUrl,
+      method: 'GET',
+      query: queryString.stringify(queryParams, {arrayFormat: 'bracket'})
+    })
   }
 
   async function * list (endpoint, queryParams = {}) {
@@ -70,21 +65,24 @@ module.exports = (apiUrl, apiKey, options = {}) => {
 
   async function * listPaginated (endpoint, queryParams = {}) {
     try {
-      let url = resolve(endpoint)
       let query = queryString.stringify(queryParams, {arrayFormat: 'bracket'})
+      let first = await canvasGot.get({
+        query,
+        url: endpoint,
+        baseUrl: apiUrl
+      })
+
+      yield first.body
+      let url = first.headers && first.headers.link && getNextUrl(first.headers.link)
 
       while (url) {
         log(`Request GET ${url}`)
 
-        const response = await canvasGot.get({
-          query,
-          url
-        })
+        const response = await canvasGot.get({url})
 
         log(`Response from GET ${url}`)
         yield response.body
         url = response.headers && response.headers.link && getNextUrl(response.headers.link)
-        query = null
       }
     } catch (err) {
       throw removeToken(err)
